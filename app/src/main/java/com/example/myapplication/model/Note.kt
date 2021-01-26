d87f17a5c6376e452f96f277e4b9561085c98ba1
Lyapk <Elena525658>
2021-01-26 11:13:46 +0300

    Added logic and new activity

--- app/src/main/java/com/example/myapplication/model/Note.kt
+++ app/src/main/java/com/example/myapplication/model/Note.kt
@@ -1,5 +1,48 @@
 package com.example.myapplication.model
 
-class Note (val title: String, val note: String, val color: Int)  {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.util.*
 
+@Parcelize
+data class Note(
+        val id: String,
+        val title: String,
+        val note: String,
+        val color: Color = Color.WHITE,
+        val lastChanged: Date = Date()
+) : Parcelable {
+
+    companion object {
+        fun createNewNote(title: String, body: String): Note = Note(generateId(), title, body)
+
+        fun generateId(): String {
+            return UUID.randomUUID().toString()
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
+
+enum class Color() {
+    WHITE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    RED,
+    VIOLET,
+    PINK
 }
--- app/src/main/java/com/example/myapplication/ui/MainViewState.kt
+++ app/src/main/java/com/example/myapplication/ui/MainViewState.kt
@@ -2,5 +2,5 @@ package com.example.myapplication.ui
 
 import com.example.myapplication.model.Note
 
-class MainViewState(val notes: List<Note>) {
+data class MainViewState(val notes: List<Note>) {
 }
