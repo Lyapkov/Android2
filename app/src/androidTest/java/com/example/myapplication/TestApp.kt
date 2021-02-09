ec6619f7157d18e88b64df405a98e7b857445df4
Lyapk <Elena525658>
2021-02-09 20:17:02 +0300

    Added Tests

---
+++ app/src/androidTest/java/com/example/myapplication/TestApp.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class TestApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
